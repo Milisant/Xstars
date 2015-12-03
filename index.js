3e0ae1f5deccf074b64b01727ac9c4a10932a440
siphesande <siphesande@projectcodex.co>
2015-12-03 15:20:19 +0200

    geo-get

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var app = express();
 var dbOptions = {
       host: 'localhost',
       user: 'root',
-      password: 'amila',
+      password: '08386354',
       port: 3306,
       database: 'geo_get'
 };
