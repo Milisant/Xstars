13100ab4ee38c014da74fe934b8f9365b914a6e4
Onwaba <onwaba@projectcodex.co>
2015-12-03 19:54:27 +0200

    the rank manager view

--- routes/issues.js
+++ routes/issues.js
@@ -2,29 +2,37 @@ exports.show =function (req, res, next){
     req.getConnection(function(err, connection){
         if (err) return next(err);
 		//connection.query('SELECT Qty AS AmtSold ,Sales_date, Sales_price,  product_name from Sales s INNER JOIN Products p ON s.Product_Id = p.Id ORDER BY Sales_date DESC',[], function(err, results){
-        connection.query('SELECT Issues.Id,Issues.description,Issues.speed,taxiAssociation_name,Ranks.Rank_name,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date,Issues.rank_id FROM Issues INNER JOIN Taxi_associations ON Issues.association_id = Taxi_associations.id INNER JOIN Ranks ON Issues.rank_id = Ranks.id ORDER BY Issues.date DESC',[],function(err, issues){
-           // connection.query('SELECT * from Issues',[], function(err, issues){
+        connection.query('SELECT Issues.Id,Issues.description,taxiAssociation_name,Ranks.Rank_name,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date,Issues.rank_id FROM Issues INNER JOIN Taxi_associations ON Issues.association_id = Taxi_associations.id INNER JOIN Ranks ON Issues.rank_id = Ranks.id ORDER BY Issues.date DESC',[],function(err, issuesresults){
+            if (err) return next(err);
+            connection.query('SELECT * from Ranks',[], function(err, results){
             	if (err) return next(err);
-                console.log(issues.length);
+                // console.log(issues.length);
                 res.render('home',{
-                	no_issues : issues.length === 0,
-                    issues : issues
+                    issues : issuesresults,
+                    ranks:results
             	});
             });
+        });
         });    
 	}
+
 
 
 exports.add = function (req, res, next) {
     req.getConnection(function(err, connection){
         if (err) return next(err);
         var input = JSON.parse(JSON.stringify(req.body));
+        console.log(input)
         var data = {
             description : input.description,
             date :input.date,
-            start_location_latitude : input.latitude,
-            start_location_longitude :input.longitude,
-            start_location_time:input.time
+            rank_name:input.rank_name,
+            reg_number:input.reg_number,
+            association_id: input.association_id,
+            rank_id:input.rank_id
+            //start_location_latitude : input.latitude,
+            //start_location_longitude :input.longitude,
+            //start_location_time:input.time
             
             //end_location_latitude:input.latitude,
             //end_location_longitude: input.longitudes,
