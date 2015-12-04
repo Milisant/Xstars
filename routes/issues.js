97f263673c0ea7867cc67d7140a9b6ecbd8c0fdb
Onwaba <onwaba@projectcodex.co>
2015-12-04 11:07:05 +0200

    the final commit

--- routes/issues.js
+++ routes/issues.js
@@ -9,10 +9,9 @@ exports.issueSearch = function(req, res, next){
         connection.query('SELECT * FROM Ranks WHERE rank_name LIKE ?',searchVar, function(error, results) {
             if (error) return next(error);
                 res.render( 'search', {
-                    issues : results
-                    //layout : false
+                    issues : results,
+                    layout : false
                 });
-            //});
         });
     }); 
 };
@@ -21,33 +20,15 @@ exports.show =function (req, res, next){
     req.getConnection(function(err, connection){
         if (err) 
                 return next(err);
-        connection.query('SELECT Issues.Id,Issues.description,taxiAssociation_name,Ranks.Rank_name,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date,Issues.rank_id FROM Issues INNER JOIN Taxi_associations ON Issues.association_id = Taxi_associations.id INNER JOIN Ranks ON Issues.rank_id = Ranks.id ORDER BY Issues.date DESC',[],function(err, issuesresults){
-<<<<<<< HEAD
+        connection.query('SELECT Issues.Id, Issues.description, speed, reg_number ,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date FROM Issues ORDER BY Issues.date DESC',[],function(err, issuesresults){
+
                 if (err) return next(err);
             connection.query('SELECT * from Ranks',[], function(err, results){
             	if (err) return next(err);
                 // console.log(issues.length);
-=======
-            if (err) 
-                    return next(err);
-        connection.query('SELECT * from Ranks',[], function(err, rankList){
-            if(err)
-                return next("Error Selecting : %s ", err);
-                var issue = issuesresults[0];
-                console.log(issue)
-
-                var ranks = rankList.map(function(Rank_name){
-                    return {
-                        id : Rank_name.id,
-                        Rank_name : Rank_name.Rank_name,
-                        selected : Rank_name.id === issue.id
-                    }
-                });
-
-
->>>>>>> b4d517cd0837dd696935ea8eff82366c440b77b8
                 res.render('home',{
-                    ranks:issuesresults
+                    issues:issuesresults,
+                    ranks:results
                     });
             });
         });
@@ -59,7 +40,7 @@ exports.getIssues =function (req, res, next){
         if (err) 
                 return next(err);
     
-        connection.query('SELECT Issues.Id,Issues.description,Issues.speed,taxiAssociation_name,Ranks.Rank_name,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date,Issues.rank_id FROM Issues INNER JOIN Taxi_associations ON Issues.association_id = Taxi_associations.id INNER JOIN Ranks ON Issues.rank_id = Ranks.id ORDER BY Issues.date DESC',[],function(err, issuesresults){
+        connection.query('SELECT Issues.Id,Issues.description,Issues.speed, reg_number, taxiAssociation_name,Ranks.Rank_name,DATE_FORMAT(Issues.date, "%d/%m/%Y") as Date,Issues.rank_id FROM Issues INNER JOIN Taxi_associations ON Issues.association_id = Taxi_associations.id INNER JOIN Ranks ON Issues.rank_id = Ranks.id ORDER BY Issues.date DESC',[],function(err, issuesresults){
         if (err) 
                 return next(err);
 
@@ -75,46 +56,25 @@ exports.getIssues =function (req, res, next){
 
 exports.add = function (req, res, next) {
     req.getConnection(function(err, connection){
+
         if (err) return next(err);
         var input = JSON.parse(JSON.stringify(req.body));
-        console.log(input)
+        console.log(input);
         var data = {
             description : input.description,
             date :input.date,
-<<<<<<< HEAD
             rank_name:input.rank_name,
-=======
-            Rank_name:input.Rank_name,
->>>>>>> b4d517cd0837dd696935ea8eff82366c440b77b8
             reg_number:input.reg_number,
-            association_id: input.association_id,
-            rank_id:input.rank_id,
-            //start_location_latitude : input.latitude,
-            //start_location_longitude :input.longitude,
-            //start_location_time:input.time
-<<<<<<< HEAD
-            
-            //end_location_latitude:input.latitude,
-            //end_location_longitude: input.longitudes,
-            //end_location_time: input.time,
-=======
-            start_location_latitude : input.latitude,
-            start_location_longitude :input.longitude,
-            start_location_time:input.time,            
-            end_location_latitude:input.latitude,
-            end_location_longitude: input.longitudes,
-            end_location_time: input.time
->>>>>>> b4d517cd0837dd696935ea8eff82366c440b77b8
-            //start_location_longitude : input.latitude, 
-            //end_location_id : input.latitude,
-            //taxiAssociation_name : input.taxiAssociation_name,
-            //Rank_name: input.Rank_name
-
+            speed : input.speed,
+            association_id : input.association_id
         };      
              
         connection.query('insert into Issues set ?', data, function(err, results){
-            if (err) return next(err);
-            console.log("Error inserting : %s ",err );
+            if (err) {
+                console.log("Error inserting : %s ", err.stack );
+                return next(err);
+            }
+
             res.redirect('/issues');
         });
     });
